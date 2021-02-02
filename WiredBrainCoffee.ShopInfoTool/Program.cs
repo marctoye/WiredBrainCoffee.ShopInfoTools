4afced3887bfb08393fca48fcd5781757f1d811a
Marc Toye <marc.toye@drs.wa.gov>
2021-02-01 16:03:58 -0800

    Handle coffee shop commands

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using WiredBrainCoffee.DataAccess;
 
 namespace WiredBrainCoffee.ShopInfoTool
@@ -31,6 +32,31 @@ namespace WiredBrainCoffee.ShopInfoTool
                     foreach (var coffeeShop in coffeeShops)
                     {
                         Console.WriteLine($"> " + coffeeShop.Location);
+                    }
+                }
+                else
+                {
+                    var foundCoffeeShops = coffeeShops
+                        .Where(x => x.Location.StartsWith(line, StringComparison.OrdinalIgnoreCase))
+                        .ToList();
+
+                    if (foundCoffeeShops.Count == 0)
+                    {
+                        Console.WriteLine($"> Command ' {line}' not found");
+                    }
+                    else if (foundCoffeeShops.Count == 1)
+                    {
+                        var coffeeShop = foundCoffeeShops.Single();
+                        Console.WriteLine($"> Location: {coffeeShop.Location}");
+                        Console.WriteLine($"> Beans in stock: {coffeeShop.BeansInStockInKg} kg");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"> Multiple matching coffee shop commands found:");
+                        foreach (var coffeeType in foundCoffeeShops)
+                        {
+                            Console.WriteLine($"> {coffeeType.Location}");
+                        }
                     }
                 }
             }
